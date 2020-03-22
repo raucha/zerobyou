cc162a9f7fcc68d3c04f225cd5e018e88385a697
Akio Shigekane <hoge@hoge>
2020-03-22 19:44:20 +0900

    DBにと登録できるようになった。

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,10 +2,11 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="8">
-        <v-text-field placeholder="ここにお題を記入"></v-text-field>
+        <v-text-field v-model="new_question_text" placeholder="ここに共有する質問文を記入"></v-text-field>
       </v-col>
       <v-col cols="2">
-        <v-btn justify="end" @click="say('未実装')">登録</v-btn>
+        <v-btn justify="end" @click="addQuestion()">登録</v-btn>
+        <!-- <v-btn justify="end" @click="say(new_question_text)">登録</v-btn> -->
       </v-col>
       <!-- <v-col>
         <v-btn>送信</v-btn>
@@ -22,7 +23,7 @@
               <v-list-item-subtitle class="">
                 <v-icon size="small" @click="say('未実装')">thumb_up</v-icon>
                 {{q.upvote}}
-                <v-icon size="small"　 @click="say('未実装')">thumb_down</v-icon>
+                <v-icon size="small" @click="say('未実装')">thumb_down</v-icon>
                 {{q.downvote}}
               </v-list-item-subtitle>
             </v-list-item-content>
@@ -96,10 +97,25 @@
 </template>
 
 <script>
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDK85t09jdqfW6nWZYX-oZ8scs2nGYDQE4",
+  authDomain: "zerobyou-d781c.firebaseapp.com",
+  databaseURL: "https://zerobyou-d781c.firebaseio.com",
+  projectId: "zerobyou-d781c",
+  storageBucket: "zerobyou-d781c.appspot.com",
+  messagingSenderId: "233388658632",
+  appId: "1:233388658632:web:5080f22b983c3844db3afd",
+  measurementId: "G-1MGJHH84SV"
+};
+firebase.initializeApp(firebaseConfig);
+
 export default {
-  name: "HelloWorld",
+  name: "MainPage",
 
   data: () => ({
+    new_question_text: "",
     questions: [
       {
         text: "今日やって良かったことと、その理由は？",
@@ -118,62 +134,25 @@ export default {
         downvote: 2
       }
     ],
+    addQuestion(){
+      if (""==this.new_question_text) {
+        alert("登録文が空です。")
+        return
+      }
+      var question = {
+        text: this.new_question_text,
+        upvote: 0,
+        downvote: 0
+      }
+      firebase.firestore().collection("questions").add(question)
+    }
 
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/layout/pre-defined"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
   }),
   methods: {
     say: function (msg){
       alert(msg)
-    }
+    },
+    
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import firebase from "firebase"
 
 Vue.config.productionTip = false
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="/__/firebase/7.12.0/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="/__/firebase/7.12.0/firebase-analytics.js"></script>
+
+// <!-- Initialize Firebase -->
+// <script src="/__/firebase/init.js"></script>
 
 new Vue({
   vuetify,
